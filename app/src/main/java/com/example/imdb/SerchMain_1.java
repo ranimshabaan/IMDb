22e6a339c55fd7442463075b73ba0c7a2b40bce0
romany <80491940+romanyn36@users.noreply.github.com>
2022-08-07 20:34:42 +0200

    Create SerchMain_1.java

---
+++ app/src/main/java/com/example/imdb/SerchMain_1.java
@@ -0,0 +1,14 @@
+package com.example.imdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SerchMain_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serch_main1);
+    }
+}
