a1231ff60b109fe1b6066a745ee2a2dc182c2708
ranimshabaan <110835257+ranimshabaan@users.noreply.github.com>
2022-08-10 01:05:20 +0200

    ranim

--- app/src/main/java/com/example/imdb/SerchMain_1.java
+++ app/src/main/java/com/example/imdb/SerchMain_1.java
@@ -3,6 +3,7 @@ package com.example.imdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class SerchMain_1 extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class SerchMain_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serch_main1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
